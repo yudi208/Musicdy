61f7eb82192e0bb4aae1de95f61409b6fc623c9b
Yudi <yudboxx@gmail.com>
2026-06-20 11:25:17 +0700

    change icon and app name

--- android/app/src/main/java/com/musicdy/blue/MainActivity.java
+++ android/app/src/main/java/com/musicdy/blue/MainActivity.java
@@ -1,5 +1,22 @@
 package com.musicdy.blue;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = this.bridge.getWebView();
+        WebSettings settings = webView.getSettings();
+
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+    }
+}
